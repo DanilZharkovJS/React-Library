3a34eb7b20d3df43cb5244c5b7ece6a51f696ef1
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-06-18 21:36:48 -0600

    Initial React Library App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import H1 from './components/H1'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <H1>Book Library</H1>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/H1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function H1({children}) {
+  return (
+    <div>
+      <h1>{children}</h1>
+    </div>
+  )
+}
+
+export default H1
